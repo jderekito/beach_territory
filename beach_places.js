c94c528083d3590148e983ad76ccc087ecc5e9f2
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-08-08 15:23:45 -0500

    Added a couple of beaches
    Added a couple of beaches

--- beach_places.js
+++ beach_places.js
@@ -1,12 +1,13 @@
 var bldgData = {
-	"features" : [{
+	"features" : [
+		{
 			"geometry" : { 
-				"coordinates" : [158.215072, 6.887481],
+				"coordinates" : [-86.431050, 30.384343],
 				"type" : "Point"
 			},
 			"id" : "1",
 			"properties" : {
-				"NAME" : "Perhaps 1000 BC: Polynesian Stick Maps",
+				"NAME" : "Henderson Beach State Park ",
 				"BODY" : "The Pacific Islanders of Micronesia made maps from sticks and shells to help navigate throughout the Pacific Ocean.",
                 "IMAGE" : "stickmaps.jpg", 
                 "URL"   : "http://oceantoday.noaa.gov/oceanshowsusway/"
@@ -14,220 +15,14 @@ var bldgData = {
 			"type" : "Feature"
 		}, {
 			"geometry" : { 
-				"coordinates" : [43.679291, 33.223191],
+				"coordinates" : [-86.5874231, 30.395718],
 				"type" : "Point"
 			},
 			"id" : "1",
 			"properties" : {
-				"NAME" : "600 BC: First world map",
+				"NAME" : "Wayside Park - Okaloosa Island",
 				"BODY" : "Babylonians believed to the first to make a map or attempt to map their world. This world map dates from about 600 B.C. Here Mesopotamia is in the center of the world, and an ocean lies at the margins of the known land areas. This area is considered the cradle area of western civilization including Sumer, Babylonian and Assyrian empires.",
                 "IMAGE" : "firstworldmap.jpg"
-			},
-			"type" : "Feature"
-		}, {
-			"geometry" : {
-				"coordinates" : [104.195397, 35.861660],
-				"type" : "Point"
-			},
-			"id" : "3",
-			"properties" : {
-				"NAME" : "400 BC: Chinese discover magnetic fields",
-				"BODY" : "The Chinese also discovered magnetic fields and invented the compass. Although they did not use it for cartographic purposes, navigators later learned its value in navigation as well as map-making..",
-                "IMAGE" : "chinesemagnetic.jpg"
-			},
-			"type" : "Feature"
-		}, {
-			"geometry" : {
-				"coordinates" : [23.727539, 37.983810],
-				"type" : "Point"
-			},
-			"id" : "2",
-			"properties" : {
-				"NAME" : "384-322 BC: Aristotle on a spherical earth",
-				"BODY" : "Aristotle believes earth's sphericity can be proven through visual observation. Aristotle believes earth's sphericity can be proven through visual observation. In the 4th century B.C., the earth began to be visualized as a sphere, the most perfect of all forms rather than as a disc shown here. Aristotle was influenced by the earlier work of Anaximander in the in the 6th century BC",
-                "IMAGE" : "aristotle.jpg"
-			},
-			"type" : "Feature"
-		}, {
-			"geometry" : {
-				"coordinates" : [29.918739, 31.200092],
-				"type" : "Point"
-			},
-			"id" : "3",
-			"properties" : {
-				"NAME" : "300 BC: Euclid writes Elements",
-				"BODY" : "With Euclid's Elements around 300 BC you have the introduction of a math system (Geometry) which would be built upon by others to make specific discoveries pivotal to cartographic thought. For instance Eratosthenes would use the Euclid’s to approximate to within 14-19% of accuracy the circumference of the earth.",
-                "IMAGE" : "euclid.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [32.899829, 24.088938],
-				"type" : "Point"
-			},
-			"id" : "4",
-			"properties" : {
-				"NAME" : "276-195 BC: Eratosthenes",
-				"BODY" : "Eratosthenes makes first scientific estimate of earth's circumference",
-                "IMAGE" : "eratosthenes.jpg",
-                "URL" : "https://vimeo.com/70467220"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [28.217483, 36.434963],
-				"type" : "Point"
-			},
-			"id" : "4",
-			"properties" : {
-				"NAME" : "190-125 BC: Graticule",
-				"BODY" : "Hipparchus, a predecessor of Eratosthenes, had an observatory on the Greek island of Rhodes where he made many of these discoveries. Hipparchus proposed a set of equally spaced east and west lines called parallels, or latitude, and added a set of north/south lines called meridians (or longitude) that converged at the poles… which we now call the graticule. Parallels are constantly parallel, and meridians converge at the poles.",
-                "IMAGE" : "graticule.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [29.918739, 31.200092],
-				"type" : "Point"
-			},
-			"id" : "5",
-			"properties" : {
-				"NAME" : "150-125 BC: Ptolemy",
-				"BODY" : "Ptolemy first to apply the scientific principles to the idea of mapping in a book called Geography… put in to a book the ideas of scale and other important cartographic principles such as scale, perspective and the use of grids…. Archived at the library in Alexandria, Egypt, which was burned 391 by Christian mobs… around this time we have the start of what is often referred to as the “middle ages”. Ptolemy himself was a part of a royal family which ruled for a while in Egypt, the Ptolemaic dynasty, which included the famous female ruler Cleopatra.",
-                "IMAGE" : "ptolemy.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [29.918739, 31.200092],
-				"type" : "Point"
-			},
-			"id" : "6",
-			"properties" : {
-				"NAME" : "391: Alexandria Library Burned",
-				"BODY" : "Christian mobs burned the library at Alexandra. After Ptolemy, there was a steady decline in cartography, and it disappeared from the Western world until the Middle Ages.   The Romans were interested mainly in practical maps, such as military maps. The romans were the first to make road maps as their empire expanded. The Romans also revived the ‘flat disc’ theory of the shape of the earth. As it turns out copies of Ptolemy’s Geography that had been rescued before the raids on Alexandria would be rediscovered in Constantinople, in Modern day Turkey, in 1300.",
-                "IMAGE" : "burninglibrary.jpg"
-			},
-			"type" : "Feature"  
-		},  {
-			"geometry" : {
-				"coordinates" : [-5.984459, 37.389092],
-				"type" : "Point"
-			},
-			"id" : "6",
-			"properties" : {
-				"NAME" : "600-1400: T-O Maps",
-				"BODY" : "Credited with being started by Isidore of Seville, a bishop in Spain, where The T is the Mediterranean, dividing the three continents, Asia, Europe and Africa, and the O is the surrounding Ocean. Jerusalem was generally represented in the center of the map. Asia was typically the size of the other two continents combined and the map was oriented towards the east",
-                "IMAGE" : "t_o_map.jpg"
-			},
-			"type" : "Feature"  
-		},  {
-			"geometry" : {
-				"coordinates" : [-77.39628, 25.034280],
-				"type" : "Point"
-			},
-			"id" : "7",
-			"properties" : {
-				"NAME" : "1492: Columbus Sails",
-				"BODY" : "While Ptolemy had understood the that the earth was spherical he drastically understated the circumference of the earth. Based on this misunderstanding the Italian navigator Columbus was convinced that Asia was closer that it was and instead landed in what is now known of as the Bahamas.",
-                "IMAGE" : "columbus.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [12.315515, 45.440847],
-				"type" : "Point"
-			},
-			"id" : "7",
-			"properties" : {
-				"NAME" : "1300: Portolan Charts",
-				"BODY" : "in the 1300s Italian geographer Pietro Vesconte pioneers the usage of portolan charts, which are navigational maps based on compass directions and estimated distances observed by the pilots at sea. The portolan charts are characterized by a series of straight lines, or rhumb lines, that represent compass bearings.",
-                "IMAGE" : "portolan.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [6.773876, 51.431708],
-				"type" : "Point"
-			},
-			"id" : "8",
-			"properties" : {
-				"NAME" : "1569: Mercator's Projection",
-				"BODY" : "During the Renaissance Gerardus Mercator, of the famous Belgium school of cartography, comes up with the projection that made it possible for navigators to draw straight lines between two points and use compasses to navigate between those points. The poles are distorted e.g. Greenland.",
-                "IMAGE" : "mercator.gif"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [2.352222, 48.856614],
-				"type" : "Point"
-			},
-			"id" : "9",
-			"properties" : {
-				"NAME" : "1669: First Topographic Map",
-				"BODY" : "Cassini family hired to map France by means of geometric principles of triangulation… this job took until 1745 when it was published as a map set that provided the first basis for large scale topopgraphic mapping.",
-                "IMAGE" : "firsttopo.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [-0.127758, 51.507351],
-				"type" : "Point"
-			},
-			"id" : "10",
-			"properties" : {
-				"NAME" : "1700: Tracking Longitude on the Sea",
-				"BODY" : "John Harrison, a self-educated English carpenter and clockmaker, solves the problem of calculating longitude while at sea.",
-                "IMAGE" : "longitude.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [-87.629798, 41.878114],
-				"type" : "Point"
-			},
-			"id" : "11",
-			"properties" : {
-				"NAME" : "1856 - present: Rand McNally Maps",
-				"BODY" : "William Rand and Andrew McNally in the railroad boomtown started creating foldout maps of the nations train routes. Later, Rand McNally creating road maps for gas stations in America as the automobile becomes the primary mode of transportation in America.",
-                "IMAGE" : "randmcnally.jpg"
-			},
-			"type" : "Feature"
-		},  {
-			"geometry" : {
-				"coordinates" : [-80.607713, 28.392218],
-				"type" : "Point"
-			},
-			"id" : "11",
-			"properties" : {
-				"NAME" : "1966-1968: Lunar Mapping",
-				"BODY" : "The accurate mapping of the moon, begun by John F. Kennedy, ushered in the 2nd Great Age of Discovery. An accurate mapping of the moon was necessary for the Apollo missions to be able land on the moon. Five unmanned moon orbiting satellites were able to provide this mapping.",
-                "IMAGE" : "moonmapping.jpg"
-			},
-			"type" : "Feature"
-		},  { 
-			"geometry" : {
-				"coordinates" : [-120.516395, 34.758210],
-				"type" : "Point"
-			},
-			"id" : "11",
-			"properties" : {
-				"NAME" : "1972- present: Landsat",
-				"BODY" : "In 1972 the Earth Resources Technology Satellite, aka Landsat 1, was launched from Vandenberg Air Force Base, California. This has been longest-running enterprise for acquisition of satellite imagery of Earth.",
-                "IMAGE" : "landsat.jpg", 
-                "URL"   : "https://youtu.be/Ezn1ne2Fj6Y"
-			},
-			"type" : "Feature"
-		},  { 
-			"geometry" : {
-				"coordinates" : [-74.005941, 40.712784],
-				"type" : "Point"
-			},
-			"id" : "11",
-			"properties" : {
-				"NAME" : "1994- present: COMSTAT",
-				"BODY" : "NYPD begins using maps and statistics together to manage crime and policing efforts. With COMSTAT crime mapping went from pins on paper wall maps, to a digital repository of crime information for directing resources.",
-                "IMAGE" : "comstat.jpg"
 			},
 			"type" : "Feature"
 		}           
