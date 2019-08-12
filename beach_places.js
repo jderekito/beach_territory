e394c9acac46084589acaa82081c4a5e92f54c17
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-08-12 11:24:32 -0500

    updated beaches
    updated beaches

--- beach_places.js
+++ beach_places.js
@@ -2,7 +2,7 @@ var bldgData = {
 	"features" : [
 		{
 			"geometry" : { 
-				"coordinates" : [-86.431050, 30.384343],
+				"coordinates" : [-86.4454890, 30.384227],
 				"type" : "Point"
 			},
 			"id" : "1",
@@ -15,17 +15,53 @@ var bldgData = {
 			"type" : "Feature"
 		}, {
 			"geometry" : { 
-				"coordinates" : [-86.5874231, 30.395718],
+				"coordinates" : [-86.583922, 30.394299],
 				"type" : "Point"
 			},
 			"id" : "2",
 			"properties" : {
-				"NAME" : "Wayside Park - Okaloosa Island",
-				"BODY" : "Wayside beach had a very commercial feel...",
+				"NAME" : "John Beasley Park",
+				"BODY" : "John Beasley Park...",
                 "IMAGE" : "wayside_signs.jpeg"
 			},
 			"type" : "Feature"
-		}           
+		}, {
+			"geometry" : { 
+				"coordinates" : [-86.138204, 30.319000],
+				"type" : "Point"
+			},
+			"id" : "3",
+			"properties" : {
+				"NAME" : "Seaside Beach",
+				"BODY" : "Seaside Beach...",
+                "IMAGE" : "wayside_signs.jpeg"
+			},
+			"type" : "Feature"
+		}, {
+			"geometry" : { 
+				"coordinates" : [-86.129642, 30.316524],
+				"type" : "Point"
+			},
+			"id" : "4",
+			"properties" : {
+				"NAME" : "Seagrove Beach",
+				"BODY" : "Seagrove Beach...",
+                "IMAGE" : "wayside_signs.jpeg"
+			},
+			"type" : "Feature"
+		}, {
+			"geometry" : { 
+				"coordinates" : [-86.591172, 30.393329],
+				"type" : "Point"
+			},
+			"id" : "5",
+			"properties" : {
+				"NAME" : "Wayside Park Beach",
+				"BODY" : "Wayside Park Beach had a very commercial feel...",
+                "IMAGE" : "wayside_signs.jpeg"
+			},
+			"type" : "Feature"
+		}		
 	],
 	"type" : "FeatureCollection"
 };
